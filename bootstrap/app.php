8f0e34d4d08a5bad7addba95dc00be9c59b7de83
Zpro678 <0306231108@caothang.edu.vn>
2025-10-15 14:56:04 +0700

    hhh

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,10 @@ use App\Http\Middleware\DemoMiddleWare;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [
+            __DIR__.'/../routes/web.php',
+            
+        ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
