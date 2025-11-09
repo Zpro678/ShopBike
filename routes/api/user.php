c38cab761805153f9cda8d6834044832f2a31a97
0306231165 <0306231165@caothang.edu.vn>
2025-11-09 16:18:29 +0700

    Xóa tất cả file và folder trên branch Tin

--- routes/api/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\api\AuthController;
-
-Route::post('/user/register', [AuthController::class, 'register'])->name('api.user.register');
-Route::post('/user/login', [AuthController::class, 'login'])->name('api.user.login');
-
-
